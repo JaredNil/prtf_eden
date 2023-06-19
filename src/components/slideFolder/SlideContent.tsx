9ebca4398066432c9c1da4108be12e82f1c676f0
JN <jared.bozh@gmail.com>
2023-06-19 22:52:58 +0800

    add about

--- src/components/slideFolder/SlideContent.tsx
+++ src/components/slideFolder/SlideContent.tsx
@@ -21,18 +21,18 @@ const SlideContentIntro: FC = () => {
 
 	return (
 		<>
-			<div className="slide__block">
+			<section className="slide__block">
 				<div className="intro__title">
-					<p>Welcome</p>
-					<p>To</p>
+					<div className='intro__title-top'>Welcome to <br /> Andrey </div>
+					<div className='intro__title-bottom'>Bozhenkov	<br />Portfolio</div>
 				</div>
-			</div>
+			</section>
 
-			<div className="intro__block">
+			<section className="intro__block">
 				<div className="slide__block"></div>
 				<div className="slide__block"></div>
 				<div className="slide__block"></div>
-			</div>
+			</section>
 		</ >
 	);
 
@@ -42,7 +42,64 @@ const SlideContentPets: FC = () => {
 
 	return (
 		<>
-			<div className="slide__block">
+			<div className="pets">
+
+				<div className="pets__sidebar">
+					<div className="pets__sidebar-title slide__block">
+						<span>My pet project:</span>
+					</div>
+					<div className="pets__sidebar-bar">
+
+						<div className="pets__item slide__block _active">
+							<div className="item__label"><span>●</span> label</div>
+							<div className="item__text">
+								<div className="item__text-title">Project №1</div>
+								<div className="item__text-skill">TS, Redux</div>
+							</div>
+							<div className="item__link">
+								<a href="#" className='item__link-btn'>GH</a>
+							</div>
+						</div>
+
+						<div className="pets__item slide__block">
+							<div className="item__label"><span>●</span> label</div>
+							<div className="item__text">
+								<div className="item__text-title">Project №2</div>
+								<div className="item__text-skill">TS, Redux</div>
+							</div>
+							<div className="item__link">
+								<a href="#" className='item__link-btn'>GH</a>
+							</div>
+						</div>
+
+						<div className="pets__item slide__block">
+							<div className="item__label"><span>●</span> label</div>
+							<div className="item__text">
+								<div className="item__text-title">Project №3</div>
+								<div className="item__text-skill">TS, Redux</div>
+							</div>
+							<div className="item__link">
+								<a href="#" className='item__link-btn'>GH</a>
+							</div>
+						</div>
+
+						<div className="pets__item slide__block">
+							<div className="item__label"><span>●</span> label</div>
+							<div className="item__text">
+								<div className="item__text-title">Project №4</div>
+								<div className="item__text-skill">TS, Redux</div>
+							</div>
+							<div className="item__link">
+								<a href="#" className='item__link-btn'>GH</a>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+				<div className="pets__content slide__block">
+
+				</div>
 
 			</div>
 		</ >
@@ -66,11 +123,30 @@ const SlideContentTechno: FC = () => {
 const SlideContentAbout: FC = () => {
 
 	return (
-		<>
-			<div className="slide__block">
 
+		<div className="about">
+
+			<div className="about__title slide__block">	<h2>About me</h2>	</div>
+
+			<div className="about__block">
+				<div className="slide__block">
+				</div>
+				<div className="about__inform slide__block">
+					<div className="about__inform-container">
+						<div className="about__inform-title">Меня зовут Андрей Боженков</div>
+						<div className="about__inform-text">
+							<p>Я front-end TS/JS разработчик </p>
+							<p>I love creating and expressing new ideas, through web, app design, typography, and more.</p>
+							<p>I have an experience in interactive design, illustration, and more.</p>
+							<p>I love to cook, going to the beach, animals, and fries. I’m looking for my next big challenge in UX/UI and branding  :)</p>
+							<p>Check out some of <a href="#">my projects</a>.</p>
+						</div>
+					</div>
+				</div>
 			</div>
-		</ >
+
+		</div>
+
 	);
 
 }
--- src/components/slideFolder/SlideLine.tsx
+++ src/components/slideFolder/SlideLine.tsx
@@ -10,15 +10,29 @@ const SlideLine: FC<SlideContentPropsType> = ({ slideIndex }) => {
 		'SKILLS',
 		'ABOUT ME'
 	]
+	const dataSlideLineSubtext: string[] = [
+		'INTRO',
+		'MY PET PROJECT',
+		'SKILLS',
+		'ABOUT ME'
+	]
 
 
 	return (
 		<section className='slide__line'>
 			<div className="slide__line-container">
-				<span>
-					{dataSlideLine[slideIndex]}
-				</span>
+				<h2>
+					{`● ${dataSlideLine[slideIndex]} `}
+
+				</h2>
+				<div className='slide__line-subtext-1'>
+					{`${dataSlideLineSubtext[slideIndex]} `}
+				</div>
+				<div className='slide__line-subtext-2'>
+					{`${dataSlideLine[slideIndex]} `}
+				</div>
 			</div>
+
 		</section>
 	)
 }
